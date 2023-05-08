eab08a288fcbe4d33f323c5eeea25a30f58edf71
Antigen-1 <zhanghao@antigen.top>
2023-05-08 19:51:07 +0800

    add a gracket launcher

--- info.rkt
+++ info.rkt
@@ -7,3 +7,5 @@
 (define version "0.0")
 (define pkg-authors '(hin))
 (define license '(Apache-2.0 OR MIT))
+(define gracket-launcher-names '("straw"))
+(define gracket-launcher-libraries '("main.rkt"))
